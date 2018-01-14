d43e6709b8cb0e1e5466a429cce9d93f4c04dc5a
John Chin-Jew <jchinjew@gmail.com>
2018-01-14 01:22:53 -0800

    Cleaner print out

--- main.lua
+++ main.lua
@@ -1,9 +1,7 @@
 function printMemoryUsage()
-  local memUsed = (collectgarbage("count"))/1000
-  local texUsed = system.getInfo( "textureMemoryUsed" )/1000000
-  print("\n-------- MEMORY USAGE INFORMATION --------")
-  print("System Memory Used: ", string.format("%.03f", memUsed), "Mb")
-  print("Texture Memory Used: ", string.format("%.03f", texUsed), "Mb")
-  print("------------------------------------------\n")
+  print("------- MEMORY USAGE -------")
+  print("System:  ", string.format("%.03f", collectgarbage("count") * .001), "Mb")
+  print("Texture: ", string.format("%.03f", system.getInfo("textureMemoryUsed") * .000001), "Mb")
+  print("----------------------------")
   return true
 end
